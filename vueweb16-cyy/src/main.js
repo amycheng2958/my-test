a6f4f1e75209c221e90e2b70e5e1b3f99dcbfe59
cyy <790958649@qq.com>
2020-01-11 18:41:16 +0800

    vuex

--- vueweb16-cyy/src/main.js
+++ vueweb16-cyy/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import create from '../utils/create.js'
+
 // import router from './router'
 import router from './krouter'
+
+import store from './store'
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 Vue.use(create)
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
